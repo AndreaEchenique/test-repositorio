2ba09ff68bbd1dcf844e7632da017b2f1af83629
andreaechenique <andrea07echeniq@gmail.com>
2020-09-29 10:53:43 -0400

    hhhhh

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text styles={styles.title}>Primera App Juntos!</Text>
+      <Button style={styles.btn} title="Comenzamos!" />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btn: {
+    marginTop: 20,
+    width: "95%",
+    borderRadius: 10,
+  },
+  title: {
+    textAlign: "auto",
   },
 });
