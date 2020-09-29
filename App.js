c3e3474dc09ff6997c8b8da8e42c682b31845c54
hanspam <pamilerin.hans@gmail.com>
2020-09-29 13:30:52 -0400

    Mis cambios

--- App.js
+++ App.js
@@ -5,12 +5,8 @@ import { StyleSheet, Text, View, Button, Image } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
       <Text styles={styles.title}>Primera App Juntos! Agregado por Hans</Text>
-=======
-      <Text styles={styles.title}>Primera App Juntos!</Text>
       <Image source="/descarga.jpg" style={styles.img} />
->>>>>>> 6116eeb3388acf645a5e4f32efa6399ad2b67efc
       <Button style={styles.btn} title="Comenzamos!" />
       <StatusBar style="auto" />
     </View>
